e1c8ca764bdca26e8dfe2b867d50a8be8cb36fe6
Enric Puigdemont <42671892+EPuigdemont@users.noreply.github.com>
2026-04-01 21:51:14 +0200

    feat(icons): add new SVG icons and update existing components to use icon components

--- resources/views/livewire/desktop.blade.php
+++ resources/views/livewire/desktop.blade.php
@@ -50,16 +50,20 @@
 
             {{-- Desktop: individual create buttons (hidden on mobile) --}}
             <div class="hidden shrink-0 items-center gap-1 md:flex">
-                <flux:button size="sm" icon="plus" x-on:click="$dispatch('create-entity', { mode: 'diary' })">
+                <flux:button size="sm" icon="plus" x-on:click="$dispatch('create-entity', { mode: 'diary' })" title="{{ __('Diary Entry') }}">
+                    <svg class="size-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25" /></svg>
                     <span class="hidden 2xl:inline">{{ __('Diary Entry') }}</span>
                 </flux:button>
-                <flux:button size="sm" icon="plus" x-on:click="$dispatch('create-entity', { mode: 'note' })">
+                <flux:button size="sm" icon="plus" x-on:click="$dispatch('create-entity', { mode: 'note' })" title="{{ __('Note') }}">
+                    <svg class="size-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" /></svg>
                     <span class="hidden 2xl:inline">{{ __('Note') }}</span>
                 </flux:button>
-                <flux:button size="sm" icon="plus" x-on:click="$dispatch('create-entity', { mode: 'postit' })">
+                <flux:button size="sm" icon="plus" x-on:click="$dispatch('create-entity', { mode: 'postit' })" title="{{ __('Post-it') }}">
+                    <svg class="size-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25" /></svg>
                     <span class="hidden 2xl:inline">{{ __('Post-it') }}</span>
                 </flux:button>
-                <flux:button size="sm" icon="plus" x-on:click="$refs.standaloneImageInput.click()">
+                <flux:button size="sm" icon="plus" x-on:click="$refs.standaloneImageInput.click()" title="{{ __('Image') }}">
+                    <svg class="size-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909M3.75 21h16.5A2.25 2.25 0 0 0 22.5 18.75V5.25A2.25 2.25 0 0 0 20.25 3H3.75A2.25 2.25 0 0 0 1.5 5.25v13.5A2.25 2.25 0 0 0 3.75 21Z" /></svg>
                     <span class="hidden 2xl:inline">{{ __('Image') }}</span>
                 </flux:button>
                 <flux:button size="sm" icon="bell" x-on:click="$dispatch('create-entity', { mode: 'reminder' })">
@@ -639,7 +643,7 @@
     <div wire:loading.flex wire:target="imageUpload, uploadImage"
          class="fixed inset-0 z-99999 items-center justify-center bg-black/50 backdrop-blur-sm">
         <div class="flex flex-col items-center gap-3">
-            <svg class="size-10 animate-spin text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <svg class="size-10 animate-spin text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24">
                 <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
                 <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
             </svg>
