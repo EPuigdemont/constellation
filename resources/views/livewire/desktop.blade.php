27371899a90da0e0242f9c17d6403184d020bbdb
Enric Puigdemont <42671892+EPuigdemont@users.noreply.github.com>
2026-04-01 23:30:26 +0200

    improvements to layout

--- resources/views/livewire/desktop.blade.php
+++ resources/views/livewire/desktop.blade.php
@@ -66,7 +66,8 @@
                     <svg class="size-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909M3.75 21h16.5A2.25 2.25 0 0 0 22.5 18.75V5.25A2.25 2.25 0 0 0 20.25 3H3.75A2.25 2.25 0 0 0 1.5 5.25v13.5A2.25 2.25 0 0 0 3.75 21Z" /></svg>
                     <span class="hidden 2xl:inline">{{ __('Image') }}</span>
                 </flux:button>
-                <flux:button size="sm" icon="bell" x-on:click="$dispatch('create-entity', { mode: 'reminder' })">
+                <flux:button size="sm" icon="plus" x-on:click="$dispatch('create-entity', { mode: 'reminder' })" title="{{ __('Reminder') }}">
+                    <svg class="size-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" /></svg>
                     <span class="hidden 2xl:inline">{{ __('Reminder') }}</span>
                 </flux:button>
             </div>
